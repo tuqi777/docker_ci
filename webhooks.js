2b0f389b2a2c0e387a14270e8766fbe9ce449e7d
lipeng@ele-cloud.com <lipeng@ele-cloud.com>
2020-09-06 23:24:23 +0800

    修改webhooks

--- webhooks.js
+++ webhooks.js
@@ -31,7 +31,7 @@ handler.on('error', function (err) {
 
 handler.on('*', function (event) {
     console.log('Received *', event.payload.action);
-    //   run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
+      run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 })
  
 handler.on('push', function (event) {
