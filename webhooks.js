a59253a131e5854391434ca645871dfa2d31f2d6
tuqi777 <1296757985@qq.com>
2021-08-12 12:39:31 +0800

    修改密钥

--- webhooks.js
+++ webhooks.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhooks', secret: 'Jszc@123' })
+var handler = createHandler({ path: '/webhooks', secret: 'Lp1234@@' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
 function run_cmd(cmd, args, callback) {
